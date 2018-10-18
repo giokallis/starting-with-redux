31edd8c1bca05bb4ddce40d43991ca0a21dd2f6f
giokallis <giorgoskallis21@gmail.com>
2018-10-19 00:59:29 +0300

    fix store bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import store from "../js/store/index";
 import App from "./components/App";
 render(
   <Provider store={store}>
@@ -9,3 +9,6 @@ render(
   </Provider>,
   document.getElementById("app")
 );
+
+
+window.store = store;
